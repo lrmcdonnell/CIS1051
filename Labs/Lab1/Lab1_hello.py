8aea9fc8a3eb4dcce8053adfc2723c2d046cef78
Laura McDonnell <129304200+lrmcdonnell@users.noreply.github.com>
2023-11-16 12:13:46 -0500

    Delete Labs directory

--- Labs/Lab1/Lab1_hello.py
+++
@@ -1,6 +0,0 @@
-print("Mark Zuckerberg is a computer programmer who is known for co-founding the popular social media site, Facebook. Zuckerberg began writing software and using computers while he was in middle school. In 2004, while in college, Zuckerberg launched the Facebook platform along with a few of his roommates. The site gained a huge popularity and allowed Zuckerberg to amass a large amount of wealth.")
-
-input("Have you heard of this person?:")
-
-print("They are pretty cool, aren't they.")
-
--- Labs/Lab2/Lab2_Temperature.py
+++
@@ -1,9 +0,0 @@
-
-temp_C = input("""Enter a temperature in Celsius,
-and I will convert it to Fahrenheit: """)
-
-temp_C = float(temp_C)
-
-temp_F = (9/5) * temp_C + 32
-
-print("The temperature in Fahrenheit is: ", temp_F, "degrees.")
